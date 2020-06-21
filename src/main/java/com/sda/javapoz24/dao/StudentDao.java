02433b3da096f58b067457f447f48095420ba3f4
Paweł Recław <pawel.reclaw@gmail.com>
2020-06-21 12:17:08 +0200

    Student query - create table

---
+++ src/main/java/com/sda/javapoz24/dao/StudentDao.java
@@ -0,0 +1,32 @@
+package com.sda.javapoz24.dao;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Gdy tworzę instancję tej klasy to moim celem jest uzyskanie dostępu do bazy.
+ *
+ * Jeśli chcę manipulować obiektami w bazie danych, to muszę stworzyć sobie instancję tej klasy i wywołać jedną z metod CRUD.
+ */
+public class StudentDao {
+    private MysqlDBConnection connector;
+
+    public StudentDao(MysqlDBConnection connection) {
+        this.connector = connection;
+
+        createDatabaseAndTable();
+    }
+
+    private void createDatabaseAndTable() {
+        try {
+            Connection connection = connector.createConnection();
+
+
+
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/sda/javapoz24/dao/StudentQuerries.java
@@ -0,0 +1,12 @@
+package com.sda.javapoz24.dao;
+
+public interface StudentQuerries {
+    public static final String CREATE_TABLE =
+            "create table if not exists `student`  (\n" +
+                    "`id` integer not null primary key auto_increment,\n" +
+                    "`firstName` varchar(255),\n" +
+                    "`lastName` varchar(255),\n" +
+                    "`age` integer,\n" +
+                    "`awarded` boolean,\n" +
+                    "`gender` varchar(15));";
+}

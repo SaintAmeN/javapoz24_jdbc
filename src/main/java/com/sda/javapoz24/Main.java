466d50db2710f1b45fa6ee44035300a0c3926298
Paweł Recław <pawel.reclaw@gmail.com>
2020-06-21 12:22:09 +0200

    Test 1 - stworzenie bazy

--- src/main/java/com/sda/javapoz24/Main.java
+++ src/main/java/com/sda/javapoz24/Main.java
@@ -1,5 +1,8 @@
 package com.sda.javapoz24;
 
+import com.sda.javapoz24.dao.MysqlDBConnection;
+import com.sda.javapoz24.dao.StudentDao;
+
 public class Main {
     public static void main(String[] args) {
         // 1. Model - implementacja
@@ -8,7 +11,10 @@ public class Main {
         //      b. tworzymy klasę która z użyciem mysqldatasource stworzy połączenie
         // 3. Tworzymy zapytania do bazy danych.
         // 4. Tworzymy DAO
-        //
+        // -- test
+
+        StudentDao dao = new StudentDao(new MysqlDBConnection());
+        // tabela powinna zostać automatycznie stworzona.
 
     }
 }
